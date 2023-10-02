bd4c8ff0939ecdd681c92da8787c1b034bb89fc2
Shiva <shiva.khatri0001@gmail.com>
2023-10-02 00:43:13 -0400

    Still some work on profile posts

--- src/components/Profilepage.js
+++ src/components/Profilepage.js
@@ -6,54 +6,53 @@ import Post from './Post';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../features/userSlice';
 import FlipMove from 'react-flip-move';
+import Coverphoto from '../icons/cover_photo.jpg';
 import '../css/profile.css';
 
 const Profilepage = () => {
   const user = useSelector(selectUser);
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        if (!user || !user.email) {
-          // Handle the case where user or user.email is not defined.
-          return;
-        }
-
-        const q = query(
-          collection(db, 'posts'),
-          where('description', '==', user.email), // Filter posts by user's email
-          orderBy('timestamp', 'desc')
-        );
-
-        const unsubscribe = onSnapshot(q, (snapshot) => {
-          setPosts(
-            snapshot.docs.map((doc) => ({
-              id: doc.id,
-              data: doc.data(),
-            }))
+        if (user?.email) { // Use optional chaining to check user.email
+          const q = query(
+            collection(db, 'posts'),
+            where('description', '==', user.email),
+            orderBy('timestamp', 'desc')
           );
-        });
 
-        return unsubscribe; // Unsubscribe from the snapshot listener when the component unmounts
+          const unsubscribe = onSnapshot(q, (snapshot) => {
+            setPosts(
+              snapshot.docs.map((doc) => ({
+                id: doc.id,
+                data: doc.data(),
+              }))
+            );
+            setLoading(false); // Set loading to false when data is fetched
+          });
+
+          return unsubscribe;
+        } else {
+          setLoading(false); // Set loading to false if user.email is not available
+        }
       } catch (error) {
         console.error('Error fetching posts:', error);
+        setLoading(false); // Set loading to false in case of an error
       }
     };
 
-    // Check if user and user.email are defined before fetching data
-    if (user && user.email) {
-      fetchData();
-    }
-  }, [user]); // Add 'user' to the dependency array
+    fetchData(); // Always fetch data when the component mounts
+  }, [user]);
 
   return (
     <div className="profilepage">
       <div className="main__container">
-
-         {/* Cover Photo */}
-         <div className="cover-photo">
-          <img src="" alt="Cover Photo" />
+        {/* Cover Photo */}
+        <div className="cover-photo">
+          <img src={Coverphoto} alt="Cover Photo" />
         </div>
 
         {/* Profile Picture */}
@@ -72,24 +71,26 @@ const Profilepage = () => {
           <p>Projects</p>
         </div>
 
-
-        {/* ... (the rest of your component remains the same) */}
-        {/* Feed Section */}
-        <FlipMove>
-          {posts.map(({ id, data }) => {
-            const { name, description, message, photoUrl } = data || {};
-            return (
-              <Post
-                key={id}
-                name={name || ''}
-                description={description || ''}
-                message={message || ''}
-                photoUrl={photoUrl || ''}
-              />
-            );
-          })}
-        </FlipMove>
-        {/* ... (the rest of your component remains the same) */}
+        {/* Loading Indicator */}
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          // Feed Section
+          <FlipMove>
+            {posts.map(({ id, data }) => {
+              const { name, description, message, photoUrl } = data || {};
+              return (
+                <Post
+                  key={id}
+                  name={name || ''}
+                  description={description || ''}
+                  message={message || ''}
+                  photoUrl={photoUrl || ''}
+                />
+              );
+            })}
+          </FlipMove>
+        )}
       </div>
     </div>
   );
